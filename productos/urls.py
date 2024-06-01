b7e9dbfbef81384cea5e5ccd330b61c841011c30
Yorman <yjona@espe.edu.ec>
2024-06-01 17:34:05 -0500

    Prueba Inicial

--- productos/urls.py
+++ productos/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URLs de vistas normales
-    # Crear la URL de la vista index
+    path('', views.listar_productos, name='listar_productos'),
 ]
